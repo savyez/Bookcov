b71212f447689e5e9878cc24e9daf298a68730df
Savyez <visnuk252@gmail.com>
2026-02-27 00:19:12 +0530

    Add filepath and dependencies

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import axios from 'axios';
 import pg from 'pg';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config({path: '.env'});
 
